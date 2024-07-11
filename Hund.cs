9964e6b0f251e1e063ce74388143f403b851d397
dwit.at <137614685+domwintaticlouddotcom@users.noreply.github.com>
2024-07-11 10:15:48 +0200

    Add files via upload

---
+++ Hund.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsTutorial
+{
+    internal class Hund
+    {
+        // Felder
+        public string name; // Achtung - in der Praxis nicht public verwenden!
+        public int alter; // Achtung - in der Praxis nicht public verwenden!
+
+
+        // Methoden
+        // Methode, die Wuff auf der Konsole ausgibt
+        public void Bellen()
+        {
+            Console.WriteLine($"Der Hund {name} bellt!");
+            Console.WriteLine("Wuff");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using CsTutorial;
+
+// Deklaration und Initialisierung
+Hund bello = new Hund();
+bello.name = "Bello";
+bello.alter = 3;
+
+// Ausgabe
+bello.Bellen();
